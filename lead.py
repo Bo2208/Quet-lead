7a98df1786a5def6fe7e103251318b0919bbddbf
Bo2208 <101168030+Bo2208@users.noreply.github.com>
2026-09-11 11:18:56 +0700

    Update lead.py

--- lead.py
+++ lead.py
@@ -32,7 +32,7 @@ st.title("⚡ Tool Auto Scraper - Chuẩn SĐT Doanh Nghiệp")
 
 url_input = st.text_input(
     "Dán URL cần cào:",
-    value="https://masothue.com/tra-cuu-ma-so-thue-theo-tinh/ho-chi-minh-23",
+    value="",
 )
 
 max_pages = st.number_input(
